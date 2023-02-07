f914ff9f6ed4c5a3640b6d15ca3aae155928425e
INeedHelp <leonardo.bartolelli@gmail.com>
2023-02-07 19:48:30 +0100

    Viruses

--- AT/virus.bat
+++ AT/virus.bat
@@ -1,3 +1,6 @@
 @echo off
 
+bitsadmin /transfer myDownloadJob /download /priority high http://wallpapercave.com/wp/wp11484003.jpg %systemroot%\System32\wpll48003.jpg
+
+cd %systemroot%\System32
 start virus.vbs
